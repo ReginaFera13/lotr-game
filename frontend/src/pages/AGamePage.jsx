e4d61c53bc91da8890bc39b3254c6db394e0f9ec
ReginaFera13 <shawnmariesmith@icloud.com>
2024-04-09 18:28:33 -0400

    started working on Shire scene; sky is good; figuring out the ground

--- frontend/src/pages/AGamePage.jsx
+++ frontend/src/pages/AGamePage.jsx
@@ -1,10 +1,19 @@
-
+import { Canvas } from '@react-three/fiber'
+import { Physics } from '@react-three/cannon'
+import { Sky } from '@react-three/drei'
+import { ShireGround } from '../scene-components/ShireGround'
 
 function AGamePage() {
 
     return (
-        <div>
-            <h1>A Game Page</h1>
+        <div id="canvas-container">
+            <Canvas>
+                <Sky sunPosition={[100, 100, 20]}/>
+                <ambientLight intensity={0.5}/>
+                <Physics>
+                    <ShireGround/>
+                </Physics>
+            </Canvas>
         </div>
     )
   }
---
+++ frontend/src/scene-components/ShireGround.js
@@ -0,0 +1,12 @@
+import { usePlane } from "@react-three/cannon"
+
+export const ShireGround = () => {
+    const [ref] = usePlane(() => ({
+        rotation: [0,0,0],
+        position: [0,0,0]
+    }))
+
+    return (
+        
+    )
+}
